5c4dc46fa453a7ddccfa4f63bea065e98584caa9
LeightonGuang <leighton.guang@icloud.com>
2026-02-02 12:12:49 +0000

    feat: new dice page

---
+++ app/(games)/dice/page.tsx
@@ -0,0 +1,11 @@
+import Example from "./components/Example";
+
+const DicePage = () => {
+  return (
+    <section className="h-screen w-full">
+      <Example />
+    </section>
+  );
+};
+
+export default DicePage;
